73ef607fbd9bbe0640637b58d8756ad0877895d5
takudzwalewantech <takudzwalewantech@gmail.com>
2024-12-26 13:38:27 +0200

    Update config.js

--- config.js
+++ config.js
@@ -26,9 +26,9 @@ function convertToBool(text, fault = 'true') {
 //--------------------------Mr Frank---------------------------------------
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID || "ENTER YOUR SUBZERO SESSION HERE",
+SESSION_ID: process.env.SESSION_ID || "mUQhwIYA#rzf5QFCIO2fQeWiAqvQexqFEQJk74VoTHbbExHi_J-E"
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", //TRUE OR FALSE
-OWNER_NUMBER: process.env.OWNER_NUMBER || "18062212660",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "263717467274",
 MODE: process.env.MODE || "public",  // PRIVATE OR PUBLIC
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
@@ -42,7 +42,7 @@ FAKE_RECORDING: process.env.FAKE_RECORDING || "false",
 AUTO_REACT: process.env.AUTO_REACT || "false",
 HEART_REACT: process.env.HEART_REACT || "false",
 OWNER_REACT: process.env.OWNER_REACT || "false",
-BOT_NAME: process.env.BOT_NAME || "『 ＳＵＢＺＥＲＯ - ＭＤ 』",
+BOT_NAME: process.env.BOT_NAME || "『 Takudzwalewantech - ＭＤ 』",
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
 };
 
